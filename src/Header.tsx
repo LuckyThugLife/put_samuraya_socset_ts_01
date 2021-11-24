1d837154f1266b0a30fa8c57a691626d6b7762e9
Rustam Sharipov <rustam87.sharipov@gmail.com>
2021-11-24 23:27:21 +0500

    started the project anew

---
+++ src/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const Header = () => {
+  return (
+     <header className={"header"}>
+         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Logo%21_Logo.svg/640px-Logo%21_Logo.svg.png" alt="logo"/>
+     </header>
+
+  );
+}
+
+export default Header;
---
+++ src/Profile.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css';
+
+const Profile = () => {
+  return (
+        <div className={"content"}>
+            <div>
+            <img src="https://www.popphoto.com/uploads/2019/01/17/OB5MG24P4F3YXP22SK2EGLCQ3A.jpg"/>
+            </div>
+
+        <div>
+            ava + discription
+        </div>
+        <div>
+            My post
+            <div>
+                New post
+            </div>
+            <div>
+                <div>
+                    post1
+                </div>
+                <div>
+                    post2
+                </div>
+            </div>
+        </div>
+        </div>
+
+  );
+}
+
+export default Profile;
